063bede230f402d40e4fbe674c2e872c009e43ad
Brayan Daniel Perez Pinto <brayanperezpinto.gmail.com>
2025-02-26 17:06:18 -0600

    primera version

---
+++ _1_estructraBasica/ProgramaPrincipal.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a=50
+    b=20
+    suma=a+b
+
+    if a>b:
+        print("a e sel mayor")
+        b = 100
+    else:
+        print("b es el mayor")
+
+    print(suma)
